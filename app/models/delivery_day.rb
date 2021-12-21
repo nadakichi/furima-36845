0d3b3aa42cf95241cdf9d62018be77a67cb40a23
nadakichi <shogo.oilt@gmail.com>
2021-12-21 12:01:19 +0900

    商品出品機能作成

---
+++ app/models/delivery_day.rb
@@ -0,0 +1,8 @@
+class DeliveryDay < ActiveHash::Base
+  self.data = [
+    { id: 1, delivery_day: '---' }, { id: 2, delivery_day: '1~2日で発送' }, { id: 3, delivery_day: '2~3日で発送' },
+    { id: 4, delivery_day: '4~7日で発送' }
+  ]
+  include ActiveHash::Associations
+  has_many :items
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できるとき' do
+      it '全ての項目が入力されていれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できないとき' do
+      it '商品画像必須' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名必須' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Name can't be blank")
+      end
+      it '商品説明必須' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Text can't be blank")
+      end
+      it 'カテゴリー必須' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Category can't be blank")
+      end
+      it '商品状態必須' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Condition can't be blank")
+      end
+      it '配送料の負担情報必須' do
+        @item.delivery_charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Delivery charge can't be blank")
+      end
+      it '発送元情報必須' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Prefecture can't be blank")
+      end
+      it '発送までの日数必須' do
+        @item.delivery_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Delivery day can't be blank")
+      end
+      it '価格情報必須' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price can't be blank")
+      end
+      it '価格300未満の時保存できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price can't be blank")
+      end
+      it '価格10000000以上の時保存できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price can't be blank")
+      end
+    end
+  end
+end
