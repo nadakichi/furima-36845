c8058d0c68336f83b983c740832f87e2eee0a6cb
nadakichi <shogo.oilt@gmail.com>
2021-12-22 19:20:29 +0900

    商品一覧機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,18 +12,17 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options format: { with: /\A[0-9]+\z/ } do
-    validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "can't be blank"},
-    presence: {message: "can't be blank"}
+    validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "can't be blank" },
+                      presence: { message: "can't be blank" }
   end
 
   validates :name, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :user, presence: true
   validates :image, presence: true
-
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name           {"hoge"}
-    text           {"説明"}
-    price          {1000}
-    condition_id   {2}
-    category_id   {2}
-    delivery_charge_id   {2}
-    delivery_day_id   {2}
-    prefecture_id   {2}
+    name           { 'hoge' }
+    text           { '説明' }
+    price          { 1000 }
+    condition_id   { 2 }
+    category_id { 2 }
+    delivery_charge_id { 2 }
+    delivery_day_id { 2 }
+    prefecture_id { 2 }
 
     association :user
 
