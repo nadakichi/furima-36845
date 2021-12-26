5f6e9e94fdd95be87aeee7c9247e5dc04f610a84
nadakichi <shogo.oilt@gmail.com>
2021-12-26 22:12:26 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user.id
+      redirect_to action: :index
+    end
   end
 
   def update
