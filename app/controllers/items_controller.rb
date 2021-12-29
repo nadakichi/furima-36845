2b7eb9c8600c61ad83437d3691bff5aec4c0e34e
nadakichi <shogo.oilt@gmail.com>
2021-12-29 13:15:06 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
-  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
 
   def index
