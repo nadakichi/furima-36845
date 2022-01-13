6f56a1a07ac8386a1a9c543e19209c2f5bd19bc4
nadakichi <shogo.oilt@gmail.com>
2022-01-13 20:29:08 +0900

    商品購入機能の修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
   factory :order_address do
     token { 'tok_abcdefghijk00000000000000000' }
-    user_id { 1 }
-    item_id { 1 }
     post_code { '123-4567' }
     prefecture_id { 2 }
     city { '北海道' }
